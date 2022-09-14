6ed04cf144600fd045884ae34ac8570cb53c9468
jitender-1137 <82994386+jitender-1137@users.noreply.github.com>
2022-09-15 00:39:40 +0530

    Initial commit

---
+++ src/main/java/com/totalFree/totalFree/TotalFreeApplication.java
@@ -0,0 +1,13 @@
+package com.totalFree.totalFree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalFreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalFreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totalFree/totalFree/TotalFreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totalFree.totalFree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalFreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
