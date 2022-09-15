aaf012adc0f3aff671e4866cd63d9f1b25e6ae3c
jitender-1137 <jitender1137@gmail.com>
2022-09-15 21:44:52 +0530

    model class

--- src/main/java/com/totalFree/totalFree/model/UserModel.java
+++ src/main/java/com/totalFree/totalFree/model/UserModel.java
@@ -1,6 +1,45 @@
 package com.totalFree.totalFree.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class UserModel {
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private boolean status;
+
+	private String username;
+
+	private String gender;
+
+	private Date dob;
+
+	private String password;
 }
